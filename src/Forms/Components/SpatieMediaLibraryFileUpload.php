7f538d64fe3f9c30a2f7c169efd8693d70e3c39d
Dan <git@danharrin.com>
2023-03-31 12:33:30 +0000

    chore: fix code style

--- src/Forms/Components/SpatieMediaLibraryFileUpload.php
+++ src/Forms/Components/SpatieMediaLibraryFileUpload.php
@@ -92,6 +92,7 @@ class SpatieMediaLibraryFileUpload extends FileUpload
 
         $this->saveRelationshipsUsing(static function (SpatieMediaLibraryFileUpload $component) {
             $component->saveUploadedFiles();
+            $component->deleteAbandonedFiles();
         });
 
         $this->saveUploadedFileUsing(static function (SpatieMediaLibraryFileUpload $component, TemporaryUploadedFile $file, ?Model $record): ?string {
@@ -123,16 +124,6 @@ class SpatieMediaLibraryFileUpload extends FileUpload
                 ->toMediaCollection($component->getCollection(), $component->getDiskName());
 
             return $media->getAttributeValue('uuid');
-        });
-
-        $this->deleteUploadedFileUsing(static function (SpatieMediaLibraryFileUpload $component, string $file): void {
-            if (! $file) {
-                return;
-            }
-
-            $mediaClass = config('media-library.media_model', Media::class);
-
-            $mediaClass::findByUuid($file)?->delete();
         });
 
         $this->reorderUploadedFilesUsing(static function (SpatieMediaLibraryFileUpload $component, array $state): array {
@@ -195,6 +186,17 @@ class SpatieMediaLibraryFileUpload extends FileUpload
         $this->hasResponsiveImages = $condition;
 
         return $this;
+    }
+
+    public function deleteAbandonedFiles(): void
+    {
+        /** @var Model&HasMedia $record */
+        $record = $this->getRecord();
+
+        $record
+            ->getMedia($this->getCollection())
+            ->whereNotIn('uuid', array_keys($this->getState() ?? []))
+            ->each(fn (Media $media) => $media->delete());
     }
 
     public function getCollection(): string
