f29aa8377507bb97606e882f7ec765718a8945f5
Dan <git@danharrin.com>
2023-08-21 07:29:46 +0000

    Merge pull request #7939 from cheesegrits/3.x-alpha

--- src/Forms/Components/SpatieMediaLibraryFileUpload.php
+++ src/Forms/Components/SpatieMediaLibraryFileUpload.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use League\Flysystem\UnableToCheckFileExistence;
-use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+use Livewire\TemporaryUploadedFile;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\MediaCollections\FileAdder;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
